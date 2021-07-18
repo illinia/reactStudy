8404fcf91aa9d02db960738ae756cea4818d5051
illinia <67290425+illinia@users.noreply.github.com>
2021-07-18 22:19:07 +0900

    1~48

---
+++ hello-world/cra-test/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoList from './TodoList';
+import './test.css';
+
+function App() {
+  console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+  console.log(`REACT_APP_LOGIN_API = ${process.env.REACT_APP_LOGIN_API}`);
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/cra-test/src/someFile.js
@@ -0,0 +1,2 @@
+const value = '123'.padStart(5, '0');
+console.log(value);
